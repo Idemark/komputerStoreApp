5765c62b2f6af0abef3cc7a4a00871b5c97d30ec
Alexander Idemark <alexander.idemark@se.experis.com>
2021-08-04 16:24:57 +0200

    Added laptop and buy functions

--- storeApp/js/storeApp.js
+++ storeApp/js/storeApp.js
@@ -1,5 +1,5 @@
 const balanceElement = document.getElementById("balance");
-//const loanTitleElement = document.getElementById("loanTitle");
+const loanTitleElement = document.getElementById("loanTitle");
 const loanInputElement = document.getElementById("loanInput");
 const loanElement = document.getElementById("loan");
 const loanButtonElement = document.getElementById("getALoan");
@@ -14,6 +14,8 @@ const computerCostElement = document.getElementById("computerCost");
 const buyButtonElement = document.getElementById("buyButton");
 const computerDescription = document.getElementById("computerDescription");
 const computerImage = document.getElementById("computerImage");
+const computerTitleElement = document.getElementById("computerTitle");
+const computerSpecsListElement = document.getElementById("computerSpecsList");
 
 let pay = 0;
 let balance = 0;
@@ -21,38 +23,36 @@ let remainingLoan = 0;
 let loanFlag = true;
 
 const work = () => {
-    pay = pay + 100;
-    payElement.innerHTML = pay;
+    updatePay(pay + 100);
 }
 
 const bank = () => {
-    if(remainingLoan > 0) {
-        if(remainingLoan < (pay*0.1)) { 
-            payElement.innerHTML = pay-remainingLoan;
-            pay -= remainingLoan;
-            remainingLoan -= remainingLoan;
-            loanElement.innerHTML = remainingLoan;
+    if (pay > 0) {
+        if (remainingLoan > 0) {
+            if (remainingLoan < (pay * 0.1)) {
+                updatePay(pay - remainingLoan);
+                updateOutstandingLoan(remainingLoan - remainingLoan);
+            }
+            else {
+                updatePay(pay * 0.9)
+                updateOutstandingLoan((pay * 0.1));
+                balance += (pay * 0.9);
+                pay = 0;
+            }
         }
-        else{
-            payElement.innerHTML = pay*0.9;
-            remainingLoan -= (pay*0.1);
-            balance += (pay*0.9);
-            pay=0;
-            loanElement.innerHTML = remainingLoan;
+        else {
+            updateBalance(balance + pay);
+            updatePay(0);
+            loanFlag = true;
+            loanTitleElement.className = "leftSide hide";
+            loanElement.className = "rightSide hide";
         }
-    }
-    else {
-        payElement.innerHTML = 0;
-        balance += pay;
-        pay=0;
-        balanceElement.innerHTML = balance;
-        loanFlag = true;
     }
 }
 
 const loan = () => {
-    if(loanFlag === true) {
-        loanInputElement.className="show";
+    if (loanFlag === true) {
+        loanInputElement.className = "show";
         loanFlag = false;
     }
     else {
@@ -61,47 +61,62 @@ const loan = () => {
 }
 
 const getALoan = (event) => {
-        if(event.key === 'Enter') {
-            let textInput = parseInt(loanInputElement.value);
-            if(textInput <= (balance*2)){
-                remainingLoan = textInput;
-                loanElement.innerHTML = remainingLoan;
-                balance += textInput;
-                balanceElement.innerHTML = balance;
-                loanInputElement.className="hide";
-                repayButtonDisplayElement.className="show";
-            }
-            else{
-                alert("You cant loan more than double of balance.")
-            }
-        
-        }
-        else{}
-    }
-
-const repayLoan = () => {
-    if(remainingLoan > 0) {
-        if(pay > remainingLoan) {
-            pay -= remainingLoan;
-            remainingLoan = 0;
-            payElement.innerHTML = pay;
-            loanElement.innerHTML = remainingLoan;
+    if (event.key === 'Enter') {
+        let loanInput = parseInt(loanInputElement.value);
+        if (loanInput <= (balance * 2)) {
+            updateOutstandingLoan(loanInput);
+            loanTitleElement.className = "leftSide show";
+            loanElement.className = "rightSide show";
+            updateBalance(balance += loanInput);
+            loanInputElement.className = "hide";
+            repayButtonDisplayElement.className = "show";
         }
         else {
-
-            remainingLoan -= pay;
-            pay = 0;
-            payElement.innerHTML = pay;
-            loanElement.innerHTML = remainingLoan;
+            alert("You cant loan more than double of balance.")
         }
+
     }
-    else {
-        repayButtonDisplayElement.className="hide";
+    else { }
+}
+
+const repayLoan = () => {
+    if (pay > 0) {
+        if (remainingLoan > 0) {
+            if (pay > remainingLoan) {
+                updatePay(pay - remainingLoan);
+                updateOutstandingLoan(0);
+            }
+            else {
+                updateOutstandingLoan(remainingLoan - pay);
+                updatePay(0);
+            }
+        }
+        loanTitleElement.className = "leftSide hide";
+        loanElement.className = "rightSide hide";
+        repayButtonDisplayElement.className = "hide";
     }
 }
 
-let computers= [];
+const updateBalance = (num) => {
+    balanceElement.innerHTML = num;
+    balance = num;
+}
+
+const updateOutstandingLoan = (num) => {
+    loanElement.innerHTML = num;
+    remainingLoan = num;
+}
+
+const updatePay = (num) => {
+    payElement.innerHTML = num;
+    pay = num;
+}
+
+
+let computers = [];
 let computerPrice = 0;
+let defaultComputer = [];
+let computerSpecsList = [];
 
 
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
@@ -118,17 +133,48 @@ const addComputerToStore = (computer) => {
     computerElement.value = computer.id;
     computerElement.appendChild(document.createTextNode(computer.title));
     computersElement.appendChild(computerElement);
+    createSpecsList(computer.specs);
+    loadInComputer(computers);
+
 }
+
+const loadInComputer = (computers) => {
+    const selectedComputer = computers[0];
+    createSpecsList(selectedComputer.specs);
+    computerCostElement.innerText = selectedComputer.price + " Kr";
+    computerPrice = selectedComputer.price;
+    computerTitleElement.innerText = selectedComputer.title;
+    computerDescription.innerText = selectedComputer.description;
+    computerImage.src = "https://noroff-komputer-store-api.herokuapp.com/" + selectedComputer.image;
+}
+
+const createSpecsList = (specs) => {
+    computerSpecsListElement.innerText = "";
+    specs.forEach(x => addSpecsToList(x))
+}
+
+const addSpecsToList = (spec) => {
+    const computerSpec = document.createElement("li");
+    computerSpec.appendChild(document.createTextNode(spec));
+    computerSpecsListElement.appendChild(computerSpec);
+}
+
 
 const handleComputerStoreChange = e => {
     const selectedComputer = computers[e.target.selectedIndex]
-    computerSpecsElement.innerText = selectedComputer.specs;
+    createSpecsList(selectedComputer.specs);
+
 }
 
 const displayComputerPrice = e => {
     const selectedComputer = computers[e.target.selectedIndex]
-    computerCostElement.innerText = selectedComputer.price;
+    computerCostElement.innerText = selectedComputer.price + " Kr";
     computerPrice = selectedComputer.price;
+}
+
+const showComputerTitle = e => {
+    const selectedComputer = computers[e.target.selectedIndex]
+    computerTitleElement.innerText = selectedComputer.title;
 }
 
 const showComputerDescription = e => {
@@ -142,12 +188,11 @@ const showComputerImage = e => {
 }
 
 const buyComputer = () => {
-    if(balance >= computerPrice) {
-        balance -= computerPrice;
-        balanceElement.innerHTML = balance;
+    if (balance >= computerPrice) {
+        updateBalance(balance - computerPrice);
         alert("Successfully bought a computer");
     }
-    else{
+    else {
         alert("The computer is to expensive");
     }
 }
@@ -155,12 +200,13 @@ const buyComputer = () => {
 
 
 workButtonElement.addEventListener("click", work);
-bankButtonElement.addEventListener("click",bank);
-loanButtonElement.addEventListener("click",loan);
-loanInputElement.addEventListener("keypress",getALoan);
-repayButtonElement.addEventListener("click",repayLoan);
-computersElement.addEventListener("change",handleComputerStoreChange);
+bankButtonElement.addEventListener("click", bank);
+loanButtonElement.addEventListener("click", loan);
+loanInputElement.addEventListener("keypress", getALoan);
+repayButtonElement.addEventListener("click", repayLoan);
+computersElement.addEventListener("change", handleComputerStoreChange);
 computersElement.addEventListener("change", displayComputerPrice);
 computersElement.addEventListener("change", showComputerDescription);
 computersElement.addEventListener("change", showComputerImage);
-buyButtonElement.addEventListener("click",buyComputer);
+computersElement.addEventListener("change", showComputerTitle);
+buyButtonElement.addEventListener("click", buyComputer);
